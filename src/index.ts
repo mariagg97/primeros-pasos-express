a1acfe718d4986f78a2f94e40ae226480929bed6
mariagg97 <maria.gonell@gmail.com>
2023-10-31 11:21:33 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import Express from 'express';
+
+const app: Express.Application = Express();
+const PORT: number = 3000; 
+
+app.get("/", (req: Express.Request, res: Express.Response)=>{
+    res.send("<p>Hola Mundo!</p>")
+});
+
+app.listen(PORT, ()=>{
+    console.log(`App is listening at port ${PORT}`)
+});
